4829f8f339ac2508f8f03cef5894c61b3c56e6fb
jose-montenegro-romero <149119278+jose-montenegro-romero@users.noreply.github.com>
2024-11-23 00:16:04 -0500

    update code.

--- src/app/app.server-routes.ts
+++ src/app/app.server-routes.ts
@@ -1,8 +1,11 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+// import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const serverRoutes: ServerRoute[] = [
-  { path: '', renderMode: RenderMode.Client },
-  { path: '**', renderMode: RenderMode.Server },
-  // { path: 'artista/:id', renderMode: RenderMode.Client },
-  // { path: ':id', renderMode: RenderMode.Server },
-];
+// // export const serverRoutes: ServerRoute[] = [
+// //   {
+// //     path: '',
+// //     renderMode: RenderMode.Server,
+// //   },
+// //   { path: 'artista/:id', renderMode: RenderMode.Client },
+// //   { path: ':id', renderMode: RenderMode.Client },
+// //   { path: '**', renderMode: RenderMode.Client },
+// // ];
